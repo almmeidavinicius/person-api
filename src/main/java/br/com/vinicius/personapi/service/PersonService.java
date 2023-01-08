ecf513ed5a37df0d4da13ea247bf29be6f508991
almmeidavinicius <almmeidavinicius@gmail.com>
2023-01-08 11:16:27 -0300

    Implement operation to get person by id

--- src/main/java/br/com/vinicius/personapi/service/PersonService.java
+++ src/main/java/br/com/vinicius/personapi/service/PersonService.java
@@ -3,12 +3,14 @@ package br.com.vinicius.personapi.service;
 import br.com.vinicius.personapi.dto.request.PersonDTO;
 import br.com.vinicius.personapi.dto.response.MessageResponseDTO;
 import br.com.vinicius.personapi.entity.Person;
+import br.com.vinicius.personapi.exception.PersonNotFoundException;
 import br.com.vinicius.personapi.mapper.PersonMapper;
 import br.com.vinicius.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -23,17 +25,25 @@ public class PersonService {
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
-        Person personToSave = personMapper.toModel(personDTO);
 
+        Person personToSave = personMapper.toModel(personDTO);
         Person createdPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().response("Created personDTO with id = " + createdPerson.getId()).build();
     }
 
-    public List<PersonDTO> listAll(){
+    public List<PersonDTO> listAll() {
+
         List<Person> all = personRepository.findAll();
         return all.stream()
                 .map(personMapper::toDTO)
                 .toList();
     }
 
+    public PersonDTO getById(Long id) throws PersonNotFoundException {
+
+      Person person = personRepository.findById(id)
+              .orElseThrow(() -> new PersonNotFoundException(id));
+      return personMapper.toDTO(person);
+
+    }
 }
